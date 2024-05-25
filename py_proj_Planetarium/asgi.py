724bfa35891d5a31be1a6cf9464fc80e4a57c7c9
bujhm <Bujhm99@gmail.com>
2024-05-26 01:05:45 +0300

    Start app

--- py_proj_Planetarium/asgi.py
+++ py_proj_Planetarium/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'py_proj_Planetarium.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "py_proj_Planetarium.settings")
 
 application = get_asgi_application()
--- py_proj_Planetarium/wsgi.py
+++ py_proj_Planetarium/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'py_proj_Planetarium.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "py_proj_Planetarium.settings")
 
 application = get_wsgi_application()
